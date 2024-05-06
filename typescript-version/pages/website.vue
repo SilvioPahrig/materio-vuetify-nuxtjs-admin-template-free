78142e65740713d0b3b73ee4e443c9abbd6a3826
silvio pahrig <silvio.pahrig@neofonie.de>
2024-05-06 11:08:12 +0700

    add website links

--- typescript-version/pages/website.vue
+++ typescript-version/pages/website.vue
@@ -1,9 +1,171 @@
 <script setup>
 import Links from "@/views/wave/website/links.vue";
+import LinkList from "@/views/wave/LinkList.vue";
+
+const pageLinks = [
+{
+    title: 'Startseite',
+    icon: 'ri-home-4-line',
+    link: '/website/home/',
+},
+{
+    title: 'Seiten',
+    icon: 'ri-window-line',
+    link: '/website/pages/',
+},
+{
+    title: 'Startseiten-Slider',
+    icon: 'ri-carousel-view',
+    link: '/website/slider/',
+},
+{
+    title: 'Aktionen',
+    icon: 'ri-calendar-event-line',
+    link: '/events/',
+},
+{
+    title: 'Stellenangebote',
+    icon: 'ri-group-line',
+    link: '/job-offers/',
+},
+{
+    title: 'Außenansicht',
+    icon: 'ri-image-2-line',
+    link: '/pharmacy/exterior/',
+},
+{
+    title: 'Team',
+    icon: 'ri-team-line',
+    link: '/pharmacy/team/',
+},
+{
+    title: 'Service & Leistungen',
+    icon: 'ri-tools-line',
+    link: '/pharmacy/service/',
+},
+]
+
+
+const apoLinks = [
+  {
+    title: 'Adresse & Kontaktdaten',
+    icon: 'ri-map-pin-line',
+    link: '/apotheke/adresse/',
+  },
+  {
+    title: 'Benachrichtigungen',
+    icon: 'ri-notification-3-line',
+    link: '/apotheke/benachrichtigungen/',
+  },
+  {
+    title: 'Domain',
+    icon: 'ri-earth-line',
+    link: '/apotheke/domain/',
+  },
+  {
+    title: 'Layout & Logo',
+    icon: 'ri-palette-line',
+    link: '/apotheke/optik/',
+  },
+  {
+    title: 'Öffnungszeiten',
+    icon: 'ri-time-line',
+    link: '/apotheke/oeffnungszeiten/',
+  },
+  {
+    title: 'Positionierungen',
+    icon: 'ri-bubble-chart-line',
+    link: '/apotheke/positionierung/',
+  },
+  {
+    title: 'Statistik',
+    icon: 'ri-line-chart-line',
+    link: '/apotheke/statistiken/',
+  },
+  {
+    title: 'Selbstdarstellung',
+    icon: 'ri-information-line',
+    link: '/apotheke/selbstdarstellung/',
+  },
+  {
+    title: 'Social Media',
+    icon: 'ri-share-line',
+    link: '/apotheke/social_media/',
+  },
+];
+
+const legalLinks = [
+{
+    title: 'AGB',
+    icon: 'ri-file-list-3-line',
+    link: '/apotheke/agb/',
+  },
+  {
+    title: 'Datenschutz',
+    icon: 'ri-shield-user-line',
+    link: '/apotheke/datenschutz/',
+  },
+  {
+    title: 'Impressum',
+    icon: 'ri-information-line',
+    link: '/apotheke/impressum/',
+  },
+  {
+    title: 'Widerruf',
+    icon: 'ri-arrow-go-back-line',
+    link: '/apotheke/widerruf/',
+  },
+];
+
+const adminLinks = [
+  {
+    title: 'Website pausieren',
+    icon: 'ri-pause-circle-line',
+    link: '/apotheke/stop/',
+    color: 'error',
+  },
+]
 </script>
-
 <template>
-  <h1>Meine Webseite</h1>
-
-  <Links />
+  <VRow>
+    <VCol cols="12">
+      <Links />
+    </VCol>
+  <VCol cols="12" md="6">
+    <VCard>
+      <VCardTitle>Inhalte</VCardTitle>
+      <VCardSubtitle>Verwalten Sie hier die Inhalte und Einstellungen Ihrer Website.</VCardSubtitle>
+      <VCardText>
+      <LinkList :items="pageLinks"/>
+      </VCardText>
+    </VCard>
+  </VCol>
+    <VCol cols="12" md="6">
+      <VCard>
+        <VCardTitle>Meine Apotheke</VCardTitle>
+        <VCardSubtitle>Hier finden Sie alle wichtigen Einstellungen für Ihre Apotheke.</VCardSubtitle>
+        <VCardText>
+          <LinkList :items="apoLinks"/>
+        </VCardText>
+      </VCard>
+    </VCol>
+    <VCol cols="12" md="6">
+      <VCard>
+        <VCardTitle>Rechtliches</VCardTitle>
+        <VCardSubtitle>Alle rechtlich relevanten Informationen auf einem Blick.</VCardSubtitle>
+        <VCardText>
+          <LinkList :items="legalLinks"/>
+        </VCardText>
+      </VCard>
+    </VCol>
+    <VCol cols="12" md="6">
+      <VCard>
+        <VCardTitle>Admin Einstellungen</VCardTitle>
+        <VCardSubtitle>Verwalten Sie Ihre Website.</VCardSubtitle>
+        <VCardText>
+          <LinkList :items="adminLinks"/>
+        </VCardText>
+      </VCard>
+    </VCol>
+  </VRow>
 </template>
---
+++ typescript-version/views/wave/LinkList.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+import { defineProps, toRefs } from "vue";
+
+const props = defineProps({
+  items: {
+    type: Array,
+    required: true,
+  },
+});
+
+const { items } = toRefs(props)
+
+const headers = [
+  { title: 'Title', key: 'title' },
+]
+</script>
+
+<template>
+  <VDataTable
+    :items="items"
+    item-value="id"
+    class="text-no-wrap todos-table"
+    :headers="headers"
+  >
+    <template #headers/>
+    <template #bottom/>
+
+    <template #item.title="{ item }">
+      <RouterLink to="/dashboard">
+
+        <VIcon
+          size="28"
+          style="margin-right: 30px;"
+          :icon="item.icon"
+          :color="item.color ? item.color : secondary"
+        />
+        <span :class="'text-' + item.color">{{ item.title }}</span> <!-- Hier wird die Farbe aus item.color verwendet, falls vorhanden -->
+      </RouterLink>
+    </template>
+  </VDataTable>
+</template>
